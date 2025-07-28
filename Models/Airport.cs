45fcd4a9c28955d13ff092edba881acb26d07074
ireneolchan <138435391+ireneolchan@users.noreply.github.com>
2025-07-29 01:43:49 +0800

    Edited classes
    Edited classes

--- Models/Airport.cs
+++ Models/Airport.cs
@@ -6,12 +6,23 @@ namespace skylance_backend.Models;
 [Table("Airports")]
 public class Airport
 {
-    public Airport()
-    {
-        Id = Guid.NewGuid().ToString();
-    }
-    
     [Key]
     [MaxLength(255)]
-    public string Id { get; set; }
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    [Required]
+    [MaxLength(50)]
+    public required string IataCode { get; set; }
+
+    [Required]
+    [MaxLength(255)]
+    public required string Name { get; set; }
+
+    [Required]
+    [ForeignKey("CityId")]
+    public virtual required City City { get; set; }
+
+    [Required]
+    [MaxLength(255)]
+    public required string TimeZone { get; set; }
 }
