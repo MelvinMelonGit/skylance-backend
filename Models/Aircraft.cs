3394090f9c3c77b861276c1823977e2d4f8186ba
MelvinMelonGit <83698963+MelvinMelonGit@users.noreply.github.com>
2025-07-28 14:30:29 +0800

    Update Aircraft.cs
    Update Aircraft.cs

--- Models/Aircraft.cs
+++ Models/Aircraft.cs
@@ -3,8 +3,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace skylance_backend.Models;
 
-[Table("Aircrafts")]
+[Table("Aircraft")]
 public class Aircraft
 {
+    public Aircraft()
+    {
+        Id = Guid.NewGuid().ToString();
+    }
     
+    [Key]
+    public string Id { get; set; }
 }
