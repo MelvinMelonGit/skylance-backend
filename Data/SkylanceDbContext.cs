118728f50dc16ecad929516a8607858ca9acacb5
MelvinMelonGit <83698963+MelvinMelonGit@users.noreply.github.com>
2025-07-28 14:34:45 +0800

    Updated SkylanceDbContext.cs
    Updated SkylanceDbContext.cs

--- Data/SkylanceDbContext.cs
+++ Data/SkylanceDbContext.cs
@@ -1,3 +1,4 @@
+using skylance_backend.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace skylance_backend.Data;
@@ -7,11 +8,24 @@ public class SkylanceDbContext : DbContext
     public SkylanceDbContext() {}
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
         optionsBuilder.UseMySql(
-// provides database connection-string
+            // provides database connection-string
             "server=localhost;user=root;password=password;database=flipACard;",
             new MySqlServerVersion(new Version(8, 0, 36))
         );
         optionsBuilder.UseLazyLoadingProxies();
     }
-// our database tables
+    // our database tables
+    public DbSet<Aircraft> Aircraft { get; set; }
+    public DbSet<Airport> Airports { get; set; }
+    public DbSet<AppUser> AppUsers { get; set; }
+    public DbSet<AppUserSession> AppUserSessions { get; set; }
+    public DbSet<BookingDetail> BookingDetails { get; set; }
+    public DbSet<CheckInDetail> CheckInDetails { get; set; }
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<FlightBookingDetail> FlightBookingDetails { get; set; }
+    public DbSet<FlightDetail> FlightDetails { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
+    public DbSet<OverbookingDetail> OverbookingDetails { get; set; }
 }
