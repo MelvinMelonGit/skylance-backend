2278eee559449ea8fec3af8a4b4e1665e91d9436
AngelPangSL <139827053+pangsianglian@users.noreply.github.com>
2025-08-05 09:17:38 +0800

    Update SkylanceDbContext.cs

--- Data/SkylanceDbContext.cs
+++ Data/SkylanceDbContext.cs
@@ -9,7 +9,11 @@ public class SkylanceDbContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
         optionsBuilder.UseMySql(
             // provides database connection-string
+<<<<<<< Updated upstream
             "server=localhost;user=root;password=xuanL20010205;database=skylance;",
+=======
+            "server=localhost;user=root;password=Nail1925$$;database=skylance;",
+>>>>>>> Stashed changes
             new MySqlServerVersion(new Version(8, 0, 36))
         );
         optionsBuilder.UseLazyLoadingProxies();
